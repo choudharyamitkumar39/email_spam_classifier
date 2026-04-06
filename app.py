62b46f8a32d233f9e92debcf15ac8a3d6b753cb5
choudharyamitkumar39 <choudharyamitkumar39@gmail.com>
2026-04-07 02:28:21 +0530

    fixed an error

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ nltk_data_path = os.path.join(os.path.dirname(__file__), "nltk_data")
 nltk.data.path.append(nltk_data_path)
 
 nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
 nltk.download('stopwords', download_dir=nltk_data_path)
 
 # -----------------------------
